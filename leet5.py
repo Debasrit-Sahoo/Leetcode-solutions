33ee0f160d95909e6d2164f62832efb442ee58f7
Debasrit-Sahoo <236076243+Debasrit-Sahoo@users.noreply.github.com>
2025-12-29 08:53:47 +0530

    this is embarrasing

--- leet5.py
+++ leet5.py
@@ -1,20 +1,18 @@
-###NEEDS REWORK
-def longestPalindrome(s):
-        out, l = "" , len(s)
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(set(s)) == 1: return s
+        l = len(s)
+        start, length = 0, 1
+        dp = [[False]*(l+1) for _ in range(l)]
         for i in range(l):
-            temp, f, m, n = s[i], 1, 0, 0
-            if (i - 1) >= 0 and (i+1) < l:
-                left, right = s[i - 1], s[i + 1]
-                if left != right:
-                    if right == temp:
-                        n = 1
-                        temp = s[i:i+n+1]
-            while (i-f-m) >= 0 and (i+f+n) < l:
-                left, right = s[i-f-m], s[i+f+n]
-                if left == right:
-                    f += 1
-                else:
-                    break
-            m, n = (i-f-m), (i+f+n)
-            out = s[m+1:n] if n-m > len(out) else out
-        return out
+            dp[i][i+1] = True
+        for i in range(l-1):
+            if s[i] == s[i+1]:
+                start = i; length = 2 
+                dp[i][i+2] = True
+        for i in range(3, l+1):
+            for j in range(l-i+1):
+                if s[j] == s[j+i-1] and dp[j+1][j+i-1]:
+                    start = j; length = i 
+                    dp[j][j+i] = True
+        return s[start:start+length]
