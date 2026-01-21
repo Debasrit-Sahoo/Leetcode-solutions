1fccfba62fdfd04f40dc6188d0636a9e07929320
Debasrit-Sahoo <236076243+Debasrit-Sahoo@users.noreply.github.com>
2026-01-21 17:48:45 +0530

    Added leet258

---
+++ leet258_bad.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            s = 0
+            while num > 0:
+                r = num % 10
+                num //= 10
+                s += r
+            num = s
+        return num
