7ca67d44e66d86f4ba6ce6414798bd176d9ead63
Debasrit-Sahoo <236076243+Debasrit-Sahoo@users.noreply.github.com>
2026-02-27 08:06:59 +0530

    Added leet375

---
+++ leet375.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        dp = [[0]*(n+2) for _ in range(n+2)] 
+        for length in range(2, n+1):
+            for l in range(1, n-length+2):
+                r = l+length-1
+                dp[l][r] = min(x + max(dp[l][x-1],dp[x+1][r]) for x in range(l, r+1))
+        return dp[1][-2]
