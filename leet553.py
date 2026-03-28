33dafe157e3a268dde11aece9c13a7d243503128
Debasrit-Sahoo <236076243+Debasrit-Sahoo@users.noreply.github.com>
2026-03-28 13:40:50 +0530

    Added leet553.py

---
+++ leet553.py
@@ -0,0 +1,6 @@
+class Solution:
+    def optimalDivision(self, nums: List[int]) -> str:
+        n = len(nums)
+        if n == 1: return str(nums[0])
+        if n == 2: return str(nums[0]) + '/' + str(nums[1])
+        return f"{nums[0]}/(" + "/".join(map(str, nums[1:])) + ")"
