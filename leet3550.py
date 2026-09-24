545288cc4c82a64750c1e5299d7ac9a54b0541fa
Debasrit-Sahoo <236076243+Debasrit-Sahoo@users.noreply.github.com>
2026-09-24 09:27:26 +0530

    Added leet3550

---
+++ leet3550.py
@@ -0,0 +1,6 @@
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i,v in enumerate(nums):
+            if i == sum(map(int, str(v))):
+                return i
+        return -1
